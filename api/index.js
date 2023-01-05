09d0ba3ca9bc11ead55a9b733fac4a933523ed66
Leonardo Luz <leonardoluz10@hotmail.com>
2023-01-05 10:44:58 -0300

    Criando arquivos Pasta API index.js

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/teste', (req, res) => {
+res.status(200).send({mensagem: 'Boas Vindas á API'})
+})
+
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta http://localhost:${port}`)
+})
+
