22eacc35285d3f8b230d78ce32ca51b65d9d3f16
Leonardo Luz <leonardoluz10@hotmail.com>
2023-01-17 17:06:41 -0300

    Criando novos migrates

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     ativo: DataTypes.BOOLEAN,
     email: DataTypes.STRING,
     role: DataTypes.STRING
-  }, {});
+  }, {paranoid: true});
   Pessoas.associate = function(models) {
     // associations can be defined here
     Pessoas.hasMany(models.Turmas,{
